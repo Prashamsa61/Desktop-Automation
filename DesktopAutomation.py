e4bc93d8318a23a156f89496411f99a118eb0064
Prashamsa Rijal <prashamsarijal61@gmail.com>
2024-09-21 18:49:28 +0545

    Typing Text in a Field

--- DesktopAutomation.py
+++ DesktopAutomation.py
@@ -2,5 +2,8 @@ from RPA.Desktop import Desktop
 
 desktop = Desktop()
 
-#Launching an Application
+# Launching an Application
 desktop.open_application("notepad.exe")
+
+# Typing Text in a Field
+desktop.type_text("Hello,World")
