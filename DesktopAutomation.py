ff2cc68f62fcfb2470f2fdb6eb5c59a0a9decaf0
Prashamsa Rijal <prashamsarijal61@gmail.com>
2024-09-21 18:55:48 +0545

    error handling with locators

--- DesktopAutomation.py
+++ DesktopAutomation.py
@@ -7,3 +7,10 @@ desktop.open_application("notepad.exe")
 
 # Typing Text in a Field
 desktop.type_text("Hello,World")
+
+
+# Error Handling with Locators
+try:
+    desktop.click("hey.png")
+except Exception as e:
+    print(f"Error occured: {e}")
